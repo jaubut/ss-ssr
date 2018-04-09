e28ec55cd9240a308c7c5506c5036c0f0d410178
Jeremie Aubut <jeremie.aubut@gmail.com>
2018-04-09 13:53:15 -0400

    add reunion annuelle

---
+++ pages/reunionannuelle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="Don">
+    <Hero class="hero-don"><h1>Réunion Annuelle</h1></Hero>
+    <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrp3jxpXI2UHofVO?backgroundColor=green" frameborder="0" onmousewheel="" width="100%" height="671" style="background: transparent; border: 0px solid #ccc;"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  #Don {
+  width: 100%;
+  height: 100%;
+}
+
+.hero-don {
+  background-image: url(~/assets/donner.jpg);
+  background-position: center !important;
+}
+</style>
